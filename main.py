900e3c6c475ce45363c7f054a7b1110d4030594d
qwe <yaroslav.tlugachev@gmail.com>
2025-07-12 16:58:08 +0300

    edited port

--- main.py
+++ main.py
@@ -79,4 +79,4 @@ async def fill_template(
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host = "127.0.0.1", port=10000, reload=True)
+    uvicorn.run("main:app", host = "0.0.0.0", port=10000, reload=True)
